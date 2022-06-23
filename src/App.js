3f7f1a1e2b34e2b2a5ed8f77f9e74df38000b544
Ajay-selvam <ajayselvam4@gmail.com>
2022-06-23 11:01:45 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Records from "./record.json";
+// import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [setQuery] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <center>
+        <p className="title">
+          {" "}
+          <h2>Json DataTable </h2>
+        </p>{" "}
+      </center>
+
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+
+      <div className="App">
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Status</th>
+          </tr>
+
+          {Records.map((record) => (
+            <tr key={record.id}>
+              <td>{record.id}</td>
+              <td>{record.name}</td>
+              <td>{record.age}</td>
+              <td>{record.status}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
-
 export default App;
